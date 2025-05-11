c03c4da8dd518845c808f56224deb616e4d3ded0
Piyush Kumar <kumarpiyush913565@gmail.com>
2025-05-11 21:46:12 +0530

    Implement real data in default cards

--- script.js
+++ script.js
@@ -34,3 +34,39 @@ async function getWeatherDetail(city) {
     }
 }
 
+
+
+// Default cities to display
+const defaultCities = ["Paris", "Delhi", "Tokyo"];
+
+// Process each city
+defaultCities.forEach(async (city) => {
+  const weatherData = await getWeatherDetail(city);
+  defaultCards(weatherData); // Pass API data to the card function
+});
+
+
+function defaultCards (weatherData){
+    console.log(weatherData)
+    if (!weatherData) return; // Skip if API failed
+
+    const cardContainer = document.getElementById("card-container")
+    const cityCard = document.createElement("div")
+    cityCard.className = "default-card"
+
+    const iconUrl = `https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
+
+    cityCard.innerHTML = `
+    <h3 class="city-name">${weatherData.cityName}, ${weatherData.country}</h3>
+    <img src="${iconUrl}" class="icon">
+    <div class="temp">${weatherData.temperature}°C</div>
+    <div class="mainDescription">${weatherData.mainDescription}</div>
+    <div class="details">
+      Humidity ${weatherData.humidity}%
+    </div>
+  `;
+
+  cardContainer.appendChild(cityCard);
+
+
+}
